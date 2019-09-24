9dd59a131887de79b5bd5bae471bd57a81aaaeb2
Henrik789 <henrik789@github.com>
2019-09-24 14:29:08 +0200

    lade till gradient i details view

--- NotificationTest/ViewControllers/JournalTableVC/JournalTableViewController.swift
+++ NotificationTest/ViewControllers/JournalTableVC/JournalTableViewController.swift
@@ -6,11 +6,18 @@ class JournalTableViewController: UIViewController, UITableViewDelegate, UITable
     @IBOutlet weak var detailView: UIView!
     @IBOutlet weak var dateLabel: UILabel!
     @IBOutlet weak var tableView: UITableView?
+    @IBOutlet weak var detailDateLabel: UILabel!
+    @IBOutlet weak var detailHeadlineLabel: UILabel!
+    @IBOutlet weak var detailTextView: UITextView!
+    @IBOutlet weak var detailCloseButton: UIButton!
+    @IBAction func detailCloseButton(_ sender: Any) {
+        removeViewFromSuperView()
+    }
     var cloudMangaer = CloudManager()
     var notes = [CKRecord]()
     let database = CKContainer.default().privateCloudDatabase
     let dateFormatter = DateFormatter()
-
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,7 +28,9 @@ class JournalTableViewController: UIViewController, UITableViewDelegate, UITable
         tableView?.refreshControl = refreshControl
         tableView?.delegate = self
         tableView?.dataSource = self
-        queryCloud()
+        DispatchQueue.main.async {
+            self.queryCloud()
+        }
         dateFormatter.dateStyle = .medium
         dateFormatter.timeStyle = .none
         
@@ -48,6 +57,7 @@ class JournalTableViewController: UIViewController, UITableViewDelegate, UITable
     @objc func removeViewFromSuperView() {
         if let subView = self.detailView{
             subView.removeFromSuperview()
+            self.navigationController?.navigationBar.layer.zPosition = 0
             print("tar bort")
         } else {
             print("ikke!!")
@@ -57,61 +67,50 @@ class JournalTableViewController: UIViewController, UITableViewDelegate, UITable
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-
         let noteDate = notes[indexPath.row].creationDate
-        
         dateFormatter.locale = Locale(identifier: "en_US")
-
-        
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "JournalTableViewCell", for: indexPath) as? JournalTableViewCell
             else {
                 fatalError("The dequeued cell is not an instance of JournalTableViewCell")
         }
-        
         cell.dateLabel.text = dateFormatter.string(from: noteDate!)
         cell.headlineLabel.text = notes[indexPath.row].value(forKey: "journalEntry") as? String ?? "👍"
         cell.emojiLabel.text = notes[indexPath.row].value(forKey: "trainingEntry") as? String ?? "👍"
-        
         
         return cell
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        self.navigationController?.navigationBar.layer.zPosition = -1
         view.addSubview(detailView)
-        detailView.backgroundColor = .yellow
-        let noteDate = notes[indexPath.row].creationDate
-        dateLabel.text = dateFormatter.string(from: noteDate!)
         
+        let noteDate = notes[indexPath.row].creationDate
+        detailDateLabel.text = dateFormatter.string(from: noteDate!)
+        detailTextView.text = notes[indexPath.row].value(forKey: "dailyEntry") as? String ?? "👍"
+        detailHeadlineLabel.text = notes[indexPath.row].value(forKey: "journalEntry") as? String ?? "👍"
+        detailCloseButton.layer.cornerRadius = 8
+        detailCloseButton.layer.borderColor = UIColor.white.cgColor
+        detailCloseButton.layer.borderWidth = 1
         UIView.animate(withDuration: 0.8, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 5, options: .curveEaseIn, animations: {
             self.detailView.alpha = 1
             self.detailView.layer.borderColor = UIColor.gray.cgColor
             self.detailView.layer.borderWidth = 1
-            self.detailView.frame = CGRect(x: 10, y: 20, width: self.screenWidth - 20, height: self.screenHeight / 3)
+            self.detailView.frame = CGRect(x: 10, y: 20, width: self.screenWidth - 20, height: self.screenHeight * 0.9)
             self.detailView.layer.cornerRadius = 20
             self.detailView.transform = CGAffineTransform(rotationAngle: CGFloat.pi * 2)
-            self.detailView.backgroundColor = .groupTableViewBackground
+            self.detailView.backgroundColor = .white
         }) { _ in
-
+            self.detailView.isUserInteractionEnabled = true
+            let aSelector : Selector = #selector(JournalTableViewController.removeViewFromSuperView)
+            let tapGesture = UITapGestureRecognizer(target:self, action: aSelector)
+            self.detailView.addGestureRecognizer(tapGesture)
         }
-        detailView.isUserInteractionEnabled = true
-        let aSelector : Selector = #selector(JournalTableViewController.removeViewFromSuperView)
-        let tapGesture = UITapGestureRecognizer(target:self, action: aSelector)
-        detailView.addGestureRecognizer(tapGesture)
-//                newNoteTextView.translatesAutoresizingMaskIntoConstraints = false
-//        newNoteTextView.isScrollEnabled = true
-        
+
+
     }
     
+    
 
-    
-//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-//        if let indexPath = tableView?.indexPathForSelectedRow {
-//            guard let destinationVC = segue.destination as? DetailViewController else {return}
-//            let selectedRow = indexPath.row
-//            destinationVC.name = notes[selectedRow].value(forKey: "journalEntry")
-//        }
-//    }
-    
     @objc func queryCloud() {
         let query = CKQuery(recordType: "Note", predicate: NSPredicate(value: true))
         database.perform(query, inZoneWith: nil) { (records, _) in
@@ -128,88 +127,3 @@ class JournalTableViewController: UIViewController, UITableViewDelegate, UITable
     
 }
 
-
-/*
- 
- import UIKit
- import CloudKit
- 
- class JournalTableViewController: UIViewController {
- 
- @IBOutlet weak var tableView: UITableView!
- var cloudMangaer = CloudManager()
- var notes = [CKRecord]()
- let database = CKContainer.default().privateCloudDatabase
- 
- override func viewDidLoad() {
- super.viewDidLoad()
- 
- self.title = "Journal"
- tableView.register(UINib.init(nibName: JournalTableViewCell.identifier, bundle: nil), forCellReuseIdentifier: JournalTableViewCell.identifier)
- tableView.delegate = self
- tableView.dataSource = self
- tableView.register(UITableViewCell.self, forCellReuseIdentifier: JournalTableViewCell.identifier)
- 
- }
- 
- @IBAction func refreshButton(_ sender: Any) {
- queryCloud()
- DispatchQueue.main.async {
- self.tableView.reloadData()
- print(self.notes.count)
- }
- }
- 
- func queryCloud() {
- let query = CKQuery(recordType: "Note", predicate: NSPredicate(value: true))
- database.perform(query, inZoneWith: nil) { (records, _) in
- guard let records = records else {return}
- let sortedRecords = records.sorted(by: { $0.creationDate! > $1.creationDate! })
- self.notes = sortedRecords
- DispatchQueue.main.async {
- //                self.tableView.refreshControl?.endRefreshing()
- self.tableView.reloadData()
- }
- }
- }
- 
- }
- 
- extension JournalTableViewController: UITableViewDelegate, UITableViewDataSource {
- 
- 
- func numberOfSections(in tableView: UITableView) -> Int {
- // #warning Incomplete implementation, return the number of sections
- return 0
- }
- 
- func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
- // #warning Incomplete implementation, return the number of rows
- return 0
- }
- 
- 
- func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
- let cell = tableView.dequeueReusableCell(withIdentifier: JournalTableViewCell.identifier, for: indexPath) as! JournalTableViewCell
- cell.config()
- 
- return cell
- }
- 
- 
- 
- 
- 
- 
- 
- 
- 
- // In a storyboard-based application, you will often want to do a little preparation before navigation
- override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
- 
- }
- 
- 
- }
- 
- */
